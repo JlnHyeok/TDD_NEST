7926290fc2fc3e7875d331c87e39fba7eb426690
JinHyeok <gjwlsgur123@gmail.com>
2024-09-22 15:00:58 +0900

    Style: Indent 크기 변경 ( 4 -> 2 )

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -1,13 +1,11 @@
-import { Module } from "@nestjs/common";
-import { PointController } from "./point.controller";
-import { DatabaseModule } from "src/database/database.module";
+import { Module } from '@nestjs/common';
+import { PointController } from './point.controller';
+import { DatabaseModule } from 'src/database/database.module';
 import { PointService } from './point.service';
-import { Service } from './.service';
-import { PointService } from './point/point.service';
 
 @Module({
-    imports: [DatabaseModule],
-    controllers: [PointController],
-    providers: [PointService, Service],
+  imports: [DatabaseModule],
+  controllers: [PointController],
+  providers: [PointService],
 })
 export class PointModule {}
