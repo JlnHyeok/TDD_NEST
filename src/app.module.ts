36b3b07d5225d3b630f1157e1417fb2ca732d96b
JinHyeok <gjwlsgur123@gmail.com>
2024-09-22 14:49:01 +0900

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PointModule } from './point/point.module';
+import { BuyModule } from './buy/buy.module';
+
+@Module({
+  imports: [PointModule, BuyModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/point/point.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { PointController } from "./point.controller";
+import { DatabaseModule } from "src/database/database.module";
+import { PointService } from './point.service';
+import { Service } from './.service';
+import { PointService } from './point/point.service';
+
+@Module({
+    imports: [DatabaseModule],
+    controllers: [PointController],
+    providers: [PointService, Service],
+})
+export class PointModule {}
