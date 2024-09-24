fd8be811eabb415ab6e8e1be209013ec07649eee
Heo Jin Hyeok <gjwlsgur123@gmail.com>
2024-09-24 16:57:36 +0900

    Style: 불필요한 import 제거

--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  ForbiddenException,
   Get,
   Param,
   Patch,
@@ -9,8 +8,6 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { PointHistory, TransactionType, UserPoint } from './point.model';
-import { UserPointTable } from 'src/database/userpoint.table';
-import { PointHistoryTable } from 'src/database/pointhistory.table';
 import { PointBody as PointDto } from './point.dto';
 import { PointService } from './point.service';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
