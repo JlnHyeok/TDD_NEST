467c8387119c5ce6829b8139a15375d4032580fe
JinHyeok <gjwlsgur123@gmail.com>
2024-09-26 22:55:05 +0900

    Refactor: Repository 분리

--- src/point/repositories/pointhistory.repository.ts
+++ src/point/repositories/pointhistory.repository.ts
@@ -1,7 +1,11 @@
-import { PointHistoryTable } from 'src/database/pointhistory.table';
+import { PointHistoryTable } from '../../database/pointhistory.table';
 import { TransactionType, PointHistory } from '../point.model';
 import { IPointHistoryRepository } from './pointhistory.repository.interface';
+import { Injectable } from '@nestjs/common';
 
+export const POINT_HISTORY_REPOSITORY = 'POINT_HISTORY_REPOSITORY';
+
+@Injectable()
 export class PointHistoryRepository implements IPointHistoryRepository {
   constructor(private readonly pointHistoryDb: PointHistoryTable) {}
 
--- src/point/repositories/userpoint.repository.ts
+++ src/point/repositories/userpoint.repository.ts
@@ -1,6 +1,10 @@
-import { UserPointTable } from 'src/database/userpoint.table';
+import { UserPointTable } from '../../database/userpoint.table';
 import { IUserPointRepository } from './userpoint.repository.interface';
+import { Injectable } from '@nestjs/common';
 
+export const USER_POINT_REPOSITORY = 'USER_POINT_REPOSITORY';
+
+@Injectable()
 export class UserPointRepository implements IUserPointRepository {
   constructor(private readonly userDb: UserPointTable) {}
 
